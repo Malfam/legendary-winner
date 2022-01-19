551dd391a608267c754ccae7739c4f8b252b57ab
Maliheh Fazaeli <91406515+Malfam@users.noreply.github.com>
2022-01-19 18:19:50 +0100

    Add files via upload

---
+++ Working with Variables.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+A = 10
+
+
+# In[2]:
+
+
+B = 5
+
+
+# In[3]:
+
+
+#Arithmetics
+
+
+# In[4]:
+
+
+c = A + B
+
+
+# In[5]:
+
+
+D = B / A
+
+
+# ---
+
+# In[6]:
+
+
+#printing
+
+
+# In[7]:
+
+
+c           #typing a value or variable in this way is quick way of sth
+
+
+# In[10]:
+
+
+print (c)   #the proper way to use print function... print() is different to python 2.7
+
+
+# In[11]:
+
+
+print (D)   #division is different to python 2.7
+
+
+# In[12]:
+
+
+#why is it better to use print()?
+
+
+# In[14]:
+
+
+c           #without print function
+D
+
+
+# In[15]:
+
+
+print(c)    #with print function. In here, we can see the print behaviour that perform correctly
+print(D)
+
+
+# ---
+
+# In[16]:
+
+
+import math
+
+
+# In[17]:
+
+
+math.sqrt(144)
+
+
+# In[18]:
+
+
+math.sqrt(A)
+
+
+# In[19]:
+
+
+round(math.sqrt(A))
+
+
+# ---
+
+# In[21]:
+
+
+greeting = "Hello"
+name = "Bob"
+
+
+# In[27]:
+
+
+message = greeting + " " + name
+
+
+# In[28]:
+
+
+print (message)
+
